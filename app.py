af14a1e544154afa595f7edf71ea3949e73e7bd6
Emi Tsukuda <gioramlevi13@gmail.com>
2019-06-26 12:28:18 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+"""Blogly application."""
+
+
+from flask import Flask
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
---
+++ models.py
@@ -0,0 +1,25 @@
+"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+PLACEHOLDER_IMG = "https://www.dictionary.com/e/wp-content/uploads/2018/03/Upside-Down_Face_Emoji.png"
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """Create user modle for user table in db """
+    __tablename__ = "users"
+    id = db.Column(db.Integer, 
+                            primary_key=True,
+                            autoincrement=True)
+    
+    first_name = db.Column(db.String(50),
+                            nullable=False)
+             
+    last_name = db.Column(db.String(50),
+                            nullable=False)
+                   
+    image_url = db.Column(db.String, 
+                                            nullable=True,
+                                            default = PLACEHOLDER_IMG)
+        
+    def __repr__(self):
+        return f"<User id='{self.id}' name='{self.first_name} {self.last_name}'>"
