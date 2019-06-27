68ed87ebd2b6f97d4f535821594bb54a6f1d855b
Stephanie Simms <stephanie.simms@gmail.com>
2019-06-26 19:13:17 -0700

    fixed spacing and html template layouts

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 """Blogly application."""
-
-
-from flask import Flask, request, render_template, flash, redirect
+from flask import Flask, request, render_template, redirect
 from models import db, connect_db, User
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -10,7 +8,6 @@ app.config['SECRET_KEY'] = "oh-so-secret"
 
 debug = DebugToolbarExtension(app)
 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
@@ -18,50 +15,59 @@ app.config['SQLALCHEMY_ECHO'] = True
 connect_db(app)
 db.create_all()
 
+
 @app.route('/')
 def show_homepage():
-    """redirecting to / route that dispaly all of existing users """
+    """Redirect to list of users."""
     return redirect("/users")
+
 
 @app.route('/users')
 def get_users_list():
-    """ get existing users list from database and render template with the list"""
+    """Show list of all users."""
     users_list = User.query.all()
     return render_template("index.html", users=users_list)
 
+
 @app.route('/user/<user_id>')
 def show_user_info(user_id):
-    """ Show information about the given user."""
+    """Show information about the given user."""
     user_info = User.query.get(user_id)
     return render_template('user_info.html', user=user_info)
 
+
 @app.route("/users/new")
 def create_new_user_page():
-    """Show an add form for users"""
+    """Show an add form for users."""
     return render_template("create_user.html")
+
 
 @app.route("/users_new", methods=["POST"])
 def add_new_user():
-    """Process the add form, adding a new user and going back to /users"""
+    """Process the add form, adding a new user and going back to /users."""
     first_name = request.form.get("first-name")
     last_name = request.form.get("last-name")
     image_url = request.form.get("image-url")
 
-    user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    user = User(first_name=first_name,
+                last_name=last_name,
+                image_url=image_url)
     db.session.add(user)
     db.session.commit()
 
     return redirect("/users")
 
+
 @app.route("/edit_user/<user_id>")
 def show_edit_page(user_id):
-    """ Show the edit page for a user"""
+    """ Show the edit page for a user."""
     user = User.query.get(user_id)
     return render_template('edit_user.html', user=user)
 
+
 @app.route("/edit_user/<user_id>", methods=["POST"])
 def edit_user(user_id):
-    """ Process the edit form, returning the user to the /users page"""
+    """ Process the edit form, returning the user to the /users page."""
 
     first_name = request.form.get("first-name")
     last_name = request.form.get("last-name")
--- models.py
+++ models.py
@@ -1,30 +1,28 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
-PLACEHOLDER_IMG = "https://www.dictionary.com/e/wp-content/uploads/2018/03/Upside-Down_Face_Emoji.png"
 
 db = SQLAlchemy()
 
+"""Default users image."""
+PLACEHOLDER_IMG = "https://www.dictionary.com/e/wp-content/uploads/2018/03/Upside-Down_Face_Emoji.png"
+
+
 def connect_db(app):
-        db.app = app
-        db.init_app(app)
+    db.app = app
+    db.init_app(app)
+
 
 class User(db.Model):
-    """Create user modle for user table in db """
+    """Model to create a new user record."""
     __tablename__ = "users"
-    id = db.Column(db.Integer, 
-                            primary_key=True,
-                            autoincrement=True)
-    
-    first_name = db.Column(db.String(50),
-                            nullable=False)
-             
-    last_name = db.Column(db.String(50),
-                            nullable=False)
-                   
-    image_url = db.Column(db.String, 
-                                            nullable=False,
-                                            default = PLACEHOLDER_IMG)
-        
-    def __repr__(self):
-        return f"<User id='{self.id}' name='{self.first_name} {self.last_name}'>"
 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    first_name = db.Column(db.String(50), nullable=False)
+
+    last_name = db.Column(db.String(50), nullable=False)
+
+    image_url = db.Column(db.String, nullable=False, default=PLACEHOLDER_IMG)
+
+    def __repr__(self):
+        return f"< User id ='{self.id}' name='{self.first_name} {self.last_name}' >"
